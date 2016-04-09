bbe78bcd60a8d2b9d3589d3fc0ad15b53045175f
ck <ck@ck-PC.lan>
2016-04-09 22:05:38 +0800

    MorespecialService类(修改了查询多专题列表的方法)

--- cms/com/guanglumedia/cms/morespecial/service/MorespecialService.java
+++ cms/com/guanglumedia/cms/morespecial/service/MorespecialService.java
@@ -2,6 +2,7 @@ package com.guanglumedia.cms.morespecial.service;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +20,12 @@ public class MorespecialService {
 	@Autowired
 	private MorespecialDao morespecialdao;
 	
-	public PagedModelList<Morespecial> findAllMorespecialList(Morespecial morespecial, int pageNo,int pageRow) throws UnsupportedEncodingException {
-	    if (morespecial.getSpecialNames() != null) {
-            String specialNames = new String(morespecial.getSpecialNames().getBytes("ISO-8859-1"), "utf-8");
-            morespecial.setSpecialNames(specialNames);
-        }
-		List<Morespecial>            list  = this.morespecialdao.findMorespecialList(morespecial, new RowBounds((pageNo - 1) * pageRow, pageRow));
-		int                          count = this.morespecialdao.findCount(morespecial);
-		PagedModelList<Morespecial>  pml   = new PagedModelList<Morespecial>(pageNo, pageRow, count);
-		pml.addModels(list);
-		return pml;
+	public PagedModelList<Morespecial> findAllMorespecialList(int pageNo,int pageRow,Map<String,Object> map)  {
+		List<Morespecial> specialList = morespecialdao.getMorespecialList(new RowBounds((pageNo-1)*pageRow, pageRow),map);
+        int count = morespecialdao.findCount(map);
+        PagedModelList<Morespecial> page = new PagedModelList<Morespecial>(pageNo,pageRow,count);
+        page.addModels(specialList);
+        return page;
 	}
 	
 	public Morespecial getMorespecialById(int id) {
